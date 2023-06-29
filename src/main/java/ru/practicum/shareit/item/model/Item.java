762e1212a3ca39a45ff36b5ecacf08542dd6cd8a
sayvazov <seymirpro@yandex.ru>
2023-06-29 21:27:04 +0300

    feat - fixed item functionality

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -12,7 +12,7 @@ import ru.practicum.shareit.user.model.User;
 @Data
 @Builder
 public class Item {
-    private int id;
+    private Integer id;
     private String name;
     private String description;
     private Boolean available;
