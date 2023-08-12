03c341343229a3610bb355cd8ae56be551be5e93
sayvazov <seymirpro@yandex.ru>
2023-08-12 16:57:47 +0300

    feat - HW 15 implemented tests

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -3,15 +3,11 @@ package ru.practicum.shareit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 @SpringBootTest
 class ShareItTests {
 
     @Test
     void contextLoads() {
-        assertThrows(IllegalStateException.class,
-                () -> ShareItApp.main(new String[]{}));
     }
 
 }
