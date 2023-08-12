da27cdf2381b782e2d9cd90cdc872a24ef7a769a
sayvazov <seymirpro@yandex.ru>
2023-08-12 16:48:38 +0300

    feat - HW 15 implemented tests

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -1,14 +1,18 @@
 package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class ShareItTests {
 
     @Test
     void contextLoads() {
-        ShareItApp.main(new String[]{});
+        assertThrows(BeanCreationException.class,
+                () -> ShareItApp.main(new String[]{}));
     }
 
 }
